4250a5f6f5e3310b7fca1561bd6b2b9b12a7ea93
omeryildizce <omeryildizce@gmail.com>
2022-05-25 14:54:02 +0300

    Recap Demo 1
    Buton sınıfı kullanılarak satranç tahtası yapıldı.

---
+++ C_Sharp/Classes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            Console.WriteLine("-----------------");
+
+            ProductManager productManager = new ProductManager(); 
+            productManager.Add();
+            productManager.Update();
+
+            Console.WriteLine("-----------------");
+            // property
+
+            Customer customer = new Customer();
+
+            customer.Id = 1;
+            customer.FirstName = "Ali";
+            customer.LastName = "YILDIZ";
+            customer.City = "Van";
+
+            Console.WriteLine(customer.FirstName);
+
+            // 2. yol
+            Customer customer2 = new Customer 
+            {
+                Id = 2,
+                FirstName = "Ömer",
+                LastName = "Yıldız",
+                City = "Ankara"
+            };
+
+            Console.WriteLine(customer2.FirstName);
+        }
+    }
+}
