ac8196c555573dd45f9b66d98f4edc7d9b93c4f4
omeryildizce <omeryildizce@gmail.com>
2022-05-25 16:36:49 +0300

    Interface Demo
    Interface örnek uygulama yapıldı.

---
+++ C_Sharp/InterfacesDemo/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] workers = new IWorker[3]
+            {
+                new Worker(),
+                new Manager(),
+                new Robot()
+            };
+
+
+            foreach (var worker in workers)
+            {
+                worker.Work();
+            }
+
+
+            IEat[] eats = new IEat[2]
+            {
+                new Worker(),
+                new Manager()
+            };
+
+        }
+
+    }
+
+    interface IWorker
+    {
+        void Work();
+
+    }
+    interface IEat
+    {
+        void Eat();
+
+    }
+    interface ISalary
+    {
+        void Salary();
+
+    }
+
+    class Manager : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Salary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    class Worker : IWorker, ISalary, IEat
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Salary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
