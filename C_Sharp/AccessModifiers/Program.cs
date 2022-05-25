ab2d0bb4a19f5de1dc1e3967855cbbd02f9b98e5
omeryildizce <omeryildizce@gmail.com>
2022-05-25 20:50:54 +0300

    Access Modifiers
    Private, public, protected ve internal gösterimi yapıldı.

---
+++ C_Sharp/AccessModifiers/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    class Customer
+    {
+        private int IdPrivate; // Sadece tanımlandığı blokta kullanılabilir 
+        protected int IdProtected; //  kalıtım olan sınıflarda kullanılabilir
+        
+        public void Save()
+        {
+
+        }
+    }
+
+    class Student:Customer
+    {
+        public void Save()
+        {
+            Customer customer = new Customer();
+            
+            
+
+        }
+
+        public void Save2()
+        {
+            IdProtected = 1;
+
+        }
+    }
+
+    internal class Course // default internal
+    {                      // bağlı olduğu proje içinde kullanılabilir 
+        public string Name { get; set; }
+
+        // classlar, sadece iç içe olan classlarda private olabilir
+
+        private class NestedClass
+        {
+
+        }
+
+        
+    }
+
+    public class Course2
+    {
+        public int MyProperty { get; set; }
+    }
+}
---
+++ C_Sharp/AccessModifiersDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccessModifiers;
+namespace AccessModifiersDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Course2 course2 = new Course2();
+        }
+    }
+}
