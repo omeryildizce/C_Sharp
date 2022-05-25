bf5c1c4ac522a513f6d35292ae445d71177a1d67
omeryildizce <omeryildizce@gmail.com>
2022-05-25 19:10:35 +0300

    Recap Demo
    Recap Demo

---
+++ C_Sharp/RecapDemo2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecapDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Logger = new FileLogger();
+            customerManager.Add();
+        }
+    }
+
+    class CustomerManager
+    {
+        public ILogger Logger { get; set; }
+        public void Add()
+        {
+            Logger.Log(); 
+            Console.WriteLine("Customer Added");
+
+        }
+    }
+    interface ILogger 
+    {
+        void Log();
+    }
+    class DatabaseLogger:ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to database!");
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to file!");
+
+        }
+    }
+
+    class FSmsLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to sms!");
+
+        }
+    }
+}
