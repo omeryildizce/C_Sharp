812213e8cce0e5b432a8ed200803d80e9774d244
omeryildizce <omeryildizce@gmail.com>
2022-05-26 09:56:04 +0300

    ReferenceAndValue
    Reference and Value Types

---
+++ C_Sharp/ReferenceAndValue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceAndValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            int number2 = 20;
+            number2 = number1;
+            number1 = 30;
+            // number1 = 30, number2 =10
+            Console.WriteLine("Number 1:{0}, Number 2:{1}",number1, number2);
+
+            string[] cities1 = new string[] {"Adana", "Ağrı", "Ankara", "Antalya" };
+            string[] cities2 = new string[] { "Balıkesir", "Bartın", "Bursa", "Bolu" };
+            cities2 = cities1;
+            cities1[0] = "İstanbul";
+            cities1[1] = "İzmir";
+
+
+            foreach (string city in cities2)
+            {
+                Console.WriteLine(city);
+            }
+        }
+    }
+}
