a3494694261330650b28e00229a651d0433ff559
omeryildizce <omeryildizce@gmail.com>
2022-05-26 18:27:10 +0300

    Exceptions
    Exceptions

---
+++ C_Sharp/Exceptions/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ExceptionIntro();
+
+
+            HandleException(()=> 
+            {
+                Find();
+
+            }) ;
+            
+        }
+
+        private static void HandleException(Action p)
+        {
+            try
+            {
+                p.Invoke();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message); ;
+            }
+        }
+
+        private static void Find()
+        {
+            string name = "Hüseyin";
+
+            List<string> students = new List<string> { "Engin", "Ömer", "Semih" };
+            if (!students.Contains(name))
+            {
+                throw new RecordNotFound("Record not found!");
+            }
+            else
+            {
+                Console.WriteLine("Record found");
+            }
+        }
+
+        private static void ExceptionIntro()
+        {
+            try
+            {
+                string[] student = new string[3] { "Ömer", "Hüseyin", "Semih" };
+
+                student[3] = "Ahmet";
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.InnerException);
+                Console.WriteLine("Hata");
+            }
+        }
+    }
+}
---
+++ C_Sharp/Exceptions/RecordNotFound.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    public class RecordNotFound:Exception
+    {
+         
+        public RecordNotFound(string message):base(message)
+        {
+             
+        }
+    }
+}
