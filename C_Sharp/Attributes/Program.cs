c095832ff6c48bf398d0d4843ad6f72a675538bc
omeryildizce <omeryildizce@gmail.com>
2022-05-27 17:42:28 +0300

    Attributes
    Attributes işlemleri yapıldı.

---
+++ C_Sharp/Attributes/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Ali",
+                LastName = "Kaya", 
+                Age = 45
+            };
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+        }
+    }
+
+    [ToTable("Customers")]
+    class Customer
+    {
+        public int Id { get; set; }
+        [RequiredProperty]
+        public string FirstName { get; set; }
+        [RequiredProperty]
+        public string LastName { get; set; }
+        [RequiredProperty]
+        public int Age { get; set; }
+    }
+
+    class CustomerDal
+    {
+        [Obsolete("Don't use Add, instead use AddNew Method")]
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("{0}:{1} {2} - {3}", 
+                customer.Id,customer.FirstName, customer.LastName, customer.Age);
+        }
+        public void AddNew(Customer customer)
+        {
+            Console.WriteLine("{0}:{1} {2} - {3}",
+                customer.Id, customer.FirstName, customer.LastName, customer.Age);
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    class RequiredPropertyAttribute: Attribute
+    {
+
+    }
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple =true)]
+
+    class ToTableAttribute : Attribute
+    {
+        private string _tableName;
+
+        public ToTableAttribute(string tableName)
+        {
+            _tableName  = tableName;
+        }
+    }
+
+
+}
