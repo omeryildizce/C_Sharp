517c3f6b88e7f60060bf264d6ffe405a6f3f50b0
omeryildizce <omeryildizce@gmail.com>
2022-05-27 15:09:25 +0300

    EntityFrameworkDemo
    Entity Framework ile uygulama yapıldı.

---
+++ C_Sharp/EntityFrameworkDemo/ETrade_CsharpContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    public class ETrade_CsharpContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
--- C_Sharp/EntityFrameworkDemo/Product.cs
+++ C_Sharp/EntityFrameworkDemo/Product.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AdoNetDemo
+namespace EntityFrameworkDemo
 {
     public class Product
     {
