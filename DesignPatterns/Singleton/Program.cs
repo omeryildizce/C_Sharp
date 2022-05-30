f0eb961faf74e125ec68da4665dae9d18627c9d9
omeryildizce <omeryildizce@gmail.com>
2022-05-30 13:08:48 +0300

    Singleton
    Singleton

---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var customerManager = CustomerManager.CreateAsSingleton();
+            customerManager.Save();
+
+        }
+    }
+
+    class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+        private CustomerManager()
+        {
+
+        }
+
+        public static CustomerManager CreateAsSingleton()
+        {
+            if (_customerManager == null)
+	        {
+                    _customerManager = new CustomerManager();
+	        }
+            return _customerManager;
+        }
+
+        public  void Save()
+        {
+            Console.WriteLine("Saved!");
+
+        }
+    }  
+}
+
