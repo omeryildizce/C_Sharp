e6489cfb6c0f97c56d1b6e1251f02c0704bb8754
omeryildizce <omeryildizce@gmail.com>
2022-05-30 19:51:35 +0300

    Adaptor

---
+++ DesignPatterns/Facade/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Facade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Save();        }
+    }
+    class Logging : ILogging
+    {
+        public void Log()
+        {
+            Console.WriteLine("logged");
+        }
+
+    }
+
+    internal interface ILogging
+    {
+        void Log();
+    }
+    internal interface ICaching
+    {
+        void Cache();
+    }
+    internal interface IAuthorize
+    {
+        void CheckUser();
+    }
+    class Caching : ICaching
+    {
+        public void Cache()
+        {
+            Console.WriteLine("cached");
+        }
+
+    }
+    class Authorize : IAuthorize
+    {
+        public void CheckUser()
+        {
+            Console.WriteLine("user cheecked");
+        }
+
+    }
+    class CustomerManager
+    {
+        CrossCuttingConcernsFacade _concerns;
+        public CustomerManager()
+        {
+            _concerns = new CrossCuttingConcernsFacade();
+        }
+        public void Save()
+        {
+            _concerns.Caching.Cache();
+            _concerns.Logging.Log();
+            _concerns.Authorize.CheckUser();
+            Console.WriteLine("Saved");
+        }
+    }
+
+    class CrossCuttingConcernsFacade
+    {
+        public ILogging Logging;
+        public ICaching Caching;
+        public IAuthorize Authorize;
+        public CrossCuttingConcernsFacade()
+        {
+            Logging = new Logging();
+            Caching = new Caching();
+            Authorize = new Authorize();
+        }
+    }
+}
