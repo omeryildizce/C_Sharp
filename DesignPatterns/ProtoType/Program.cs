d55954f8c697b6abff40b8490182de73048e3a82
omeryildizce <omeryildizce@gmail.com>
2022-05-30 18:19:34 +0300

    ProtoType
    ProtoType

---
+++ DesignPatterns/ProtoType/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ProtoType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer() { FirstName = "ömer", LastName="YILDIZ", City="Ankara", Id= 1};
+            Console.WriteLine(customer.FirstName);
+
+            var customer2 = customer.Clone();
+            customer2.FirstName = "No Name";
+
+            var customer3 = (Customer)customer.Clone();
+            customer3.City = "Van";
+
+            Console.WriteLine(customer.FirstName);
+            Console.WriteLine(customer2.FirstName);
+
+
+
+        }
+    }
+    public abstract class Person
+    {
+        public abstract Person Clone();
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+    public class Customer : Person
+    {
+        public string City { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+
+        }
+    }
+    public class Employee : Person
+    {
+        public decimal Salary { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+
+        }
+    }
+
+}
