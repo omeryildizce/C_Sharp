42e416464676e20b716a72f0f236bfcc2cd7c127
omeryildizce <omeryildizce@gmail.com>
2022-05-31 15:52:20 +0300

    DependencyInjection

---
+++ DesignPatterns/DependcyInjection/Program.cs
@@ -0,0 +1,50 @@
+﻿using Ninject;
+using System;
+
+namespace DependencyInjection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IKernel kernel = new StandardKernel();
+            kernel.Bind<IProductDal>().To<EfProductDall>().InSingletonScope();
+
+            ProductManager productManager = new ProductManager(kernel.Get<IProductDal>());
+            productManager.Save();
+        }
+    }
+
+    interface IProductDal
+    {
+        void Save();
+    }
+    class EfProductDall : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with Ef");
+        }
+    }
+    class NhProductDall : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with Nh");
+        }
+    }
+    class ProductManager
+    {
+        private IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public void Save()
+        {
+            // Business code
+
+            _productDal.Save();
+        }
+    }
+}
