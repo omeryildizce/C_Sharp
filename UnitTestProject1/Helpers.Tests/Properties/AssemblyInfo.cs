a8672065559cfd6638f1cfb8f864ed62bcbd151d
omeryildizce <omeryildizce@gmail.com>
2022-05-31 18:52:42 +0300

    Unit Test

---
+++ UnitTestProject1/Helpers.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Helpers.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Helpers.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a1d8f802-9f5c-448f-8901-4043e1abc692")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/Helpers.Tests/StringHelperTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helpers.StringHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Helpers.Tests
+{
+    [TestClass]
+    public class StringHelperTests
+    {
+        [TestMethod]
+        public void Girilen_ifadenin_basindaki_ve_sonundaki_bosluklar_silinmelidir()
+        {
+            // Arange 
+            var ifade = "    Ömer         Yıldız   ";
+            var beklenen = " Ömer Yıldız";
+
+            //Act
+            var gerceklesen = StringHelper.FazlaBosluklariSil(ifade);
+
+            // Assert
+            Assert.AreEqual(beklenen, gerceklesen); 
+        }
+    }
+}
