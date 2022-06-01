f4de4a7fe9d893bccc94189380865aff85de335a
omeryildizce <omeryildizce@gmail.com>
2022-06-01 10:56:44 +0300

    Test

---
+++ TestAttributes/TestAttributes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestAttributes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestAttributes")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1cbda201-8900-4b21-918b-58d1570eb739")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestAttributes/TestAttributes/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestAttributes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [Owner("Ömer")]
+        [TestCategory("Tester")]
+        [Priority(1)]
+        [TestProperty("Günceleyen", "Engin")]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1,1);
+        }
+
+        [TestMethod]
+        [Owner("Ömer")]
+        [TestCategory("Tester")]
+        [Priority(1)]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        [Owner("Ali")]
+        [TestCategory("Developer")]
+        [Priority(2)]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        [Owner("Ali")]
+        [Priority(4)]
+        [TestCategory("Developer")]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        [Owner("Hande")]
+        [Priority(3)]
+        [TestCategory("Developer")]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
